1c36a73061e0e222157989bc64c95242e31c8e11
Daniel Andrés Triana Camacho <47016495+dantrica@users.noreply.github.com>
2023-03-22 21:52:49 -0500

    Update json_20191016.py

--- json_20191016.py
+++ json_20191016.py
@@ -8,35 +8,35 @@ Created on Tue Sep 10 23:39:14 2019
 json = {
     'name':'Probetas de cemento',
     'compresion':[
-            # {'path':'20191016/f_p8_1.txt', 'rate': 0.20}, #Si
-            # {'path':'20191016/f_p9_1.txt', 'rate': 0.20}, #Si
-            # {'path':'20191016/f_p10_1.txt', 'rate': 0.20},
-            # {'path':'20191016/f_p11_1.txt', 'rate': 0.20}, #signo
+            {'path':'20191016/f_p8_1.txt', 'rate': 0.20}, #Si
+            {'path':'20191016/f_p9_1.txt', 'rate': 0.20}, #Si
+            {'path':'20191016/f_p10_1.txt', 'rate': 0.20},
+            {'path':'20191016/f_p11_1.txt', 'rate': 0.20}, #signo
             {'path':'20191016/f_p12_1.txt', 'rate': 0.20}, #Si
-            # {'path':'20191016/f_p13_1.txt', 'rate': 0.20}, #Si
-            # {'path':'20191016/f_p14_1.txt', 'rate': 0.20}, #Si
-            # {'path':'20191016/f_p15_1.txt', 'rate': 0.20}, #Si
-            # {'path':'20191016/f_p16_1.txt', 'rate': 0.20}, #Si
-            # {'path':'20191016/f_p17_1.txt', 'rate': 0.20} #Si
+            {'path':'20191016/f_p13_1.txt', 'rate': 0.20}, #Si
+            {'path':'20191016/f_p14_1.txt', 'rate': 0.20}, #Si
+            {'path':'20191016/f_p15_1.txt', 'rate': 0.20}, #Si
+            {'path':'20191016/f_p16_1.txt', 'rate': 0.20}, #Si
+            {'path':'20191016/f_p17_1.txt', 'rate': 0.20} #Si
         ],
     'is':[
-            # {'path':'20191016/Z_p10_5kN.txt', 'force': 5.0},
+            {'path':'20191016/Z_p10_5kN.txt', 'force': 5.0},
             {'path':'20191016/Z_p13_c1.txt', 'force': 3.0},
-            # {'path':'20191016/Z_p14_2kN.txt', 'force': 2.0},
-            # {'path':'20191016/Z_p15_2kN.txt', 'force': 2.0},
-            # {'path':'20191016/Z_p16_c1.txt', 'force': 2.0},
-#            {'path':'20191016/Z_p17_2kN.txt', 'force': 2.0}
+            {'path':'20191016/Z_p14_2kN.txt', 'force': 2.0},
+            {'path':'20191016/Z_p15_2kN.txt', 'force': 2.0},
+            {'path':'20191016/Z_p16_c1.txt', 'force': 2.0},
+            {'path':'20191016/Z_p17_2kN.txt', 'force': 2.0}
         ],
     'ocp':[
-#            {'path':'20191016/OCP_p8_0.2kNs.txt', 'rate': 0.20},
-#            {'path':'20191016/OCP_p9_0.2kNs.txt', 'rate': 0.20},
-#            {'path':'20191016/OCP_p10_0.2kNs.txt', 'rate': 0.20},
+            {'path':'20191016/OCP_p8_0.2kNs.txt', 'rate': 0.20},
+            {'path':'20191016/OCP_p9_0.2kNs.txt', 'rate': 0.20},
+            {'path':'20191016/OCP_p10_0.2kNs.txt', 'rate': 0.20},
             {'path':'20191016/OCP_p11_0.2kNs.txt', 'rate': 0.20},
-#            {'path':'20191016/OCP_p12_0.2kNs.txt', 'rate': 0.20},
-#            {'path':'20191016/OCP_p13_0.2kNs.txt', 'rate': 0.20},
-#            {'path':'20191016/OCP_p14_0.2kNs.txt', 'rate': 0.20},
-            # {'path':'20191016/OCP_p15_0.2kNs.txt', 'rate': 0.20},
-#            {'path':'20191016/OCP_p16_0.2kNs.txt', 'rate': 0.20},
-#            {'path':'20191016/OCP_p17_0.2kNs.txt', 'rate': 0.20}
+            {'path':'20191016/OCP_p12_0.2kNs.txt', 'rate': 0.20},
+            {'path':'20191016/OCP_p13_0.2kNs.txt', 'rate': 0.20},
+            {'path':'20191016/OCP_p14_0.2kNs.txt', 'rate': 0.20},
+            {'path':'20191016/OCP_p15_0.2kNs.txt', 'rate': 0.20},
+            {'path':'20191016/OCP_p16_0.2kNs.txt', 'rate': 0.20},
+            {'path':'20191016/OCP_p17_0.2kNs.txt', 'rate': 0.20}
         ]
 }
